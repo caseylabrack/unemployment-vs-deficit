7269a2bf575ad195a51a6c0f44beefcd7b67bd7f
Casey Labrack <clabrack@gmail.com>
2019-07-06 17:54:03 -0400

    formatting and copywriting mostly

--- chart.js
+++ chart.js
@@ -1,5 +1,5 @@
 const svg = d3.select("svg"),
-  margin = {top: 70, right: 20, bottom: 50, left: 20},
+  margin = {top: 40, right: 20, bottom: 50, left: 28},
   svgWidth = d3.select("svg").property("parentNode").offsetWidth,
   width =  svgWidth - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom,
@@ -13,7 +13,9 @@ const x = d3.scaleLinear().domain([3.5, 10]).range([0,width]),
       y = d3.scaleLinear().domain([-10,5]).range([height, 0]),
       line = d3.line().x(row => x(row.unemployment)).y(row => y(row.deficit))
 
-length = lineData => d3.create("svg:path").attr("d", lineData).node().getTotalLength()
+const length = lineData => d3.create("svg:path").attr("d", lineData).node().getTotalLength()
+
+const datacolor = d3.hcl(310,50,70)
 
 d3.csv("data.csv", ({year, deficits, unemployment}) => ({year: +year, deficit: +deficits, unemployment: +unemployment}))
   .then(data => {
@@ -31,22 +33,21 @@ d3.csv("data.csv", ({year, deficits, unemployment}) => ({year: +year, deficit: +
 
     chart.append("text").text("Low Unemployment/").attr("x", x(4.25)).attr("y", y(-9)).style("text-anchor", "middle").style("font-size", ".7em").style("fill", d3.hcl(0,0,70))
       .append("tspan").text("Budget Deficit").attr("x", x(4.25)).attr("dy", "1em")
-
     chart.append("text").text("High Unemployment/").attr("x", x(8)).attr("y", y(-9)).style("text-anchor", "middle").style("font-size", ".7em").style("fill", d3.hcl(0,0,70))
       .append("tspan").text("Budget Deficit").attr("x", x(8)).attr("dy", "1em")
-
     chart.append("text").text("Low Unemployment/").attr("x", x(4.25)).attr("y", y(4)).style("text-anchor", "middle").style("font-size", ".7em").style("fill", d3.hcl(0,0,70))
       .append("tspan").text("Budget Surplus").attr("x", x(4.25)).attr("dy", "1em")
-
     chart.append("text").text("High Unemployment/").attr("x", x(8)).attr("y", y(4)).style("text-anchor", "middle").style("font-size", ".7em").style("fill", d3.hcl(0,0,70))
       .append("tspan").text("Budget Surplus").attr("x", x(8)).attr("dy", "1em")
 
-    chart.append("path").datum(data).attr("d", line).attr("id", "dataline").style("fill", "none").style("stroke", d3.hcl(310,50,60)).style("stroke-width", 4)//.style("stroke-opacity", .5)
+    chart.append("path").datum(data).attr("d", line).attr("id", "dataline").style("fill", "none").style("stroke", datacolor).style("stroke-width", 4)//.style("stroke-opacity", .5)
       .attr("stroke-dasharray", `0,${l}`).transition().duration(5000).attr("stroke-dasharray", `${l},${l}`)
       .on("end", () => d3.select("button").attr("disabled", null))
 
-    chart.append("text").attr("x", width / 2).attr("y", -(margin.top - 20)).attr("id", "yearDisplay")
-      .style("text-anchor", "end").style("font-size", "20px").style("font-family", "sans-serif").style("font-weight", 900)//.style("fill", d3.hcl(0,0,80))
+    const yearHalo = chart.append("text").attr("x", width / 2).attr("y", height/2).attr("class", "yearDisplay").style("text-anchor", "middle").style("font-size", "20px").style("font-family", "sans-serif").style("font-weight", 900).style("dominant-baseline", "middle")//.style("opacity", .5)
+    yearHalo.clone(true).style("fill", d3.hcl(310,50,40))
+    yearHalo.style("fill", "none").style("stroke", "white").style("stroke-width", 4).attr("stroke-linejoin", "round")
+    chart.selectAll(".yearDisplay")
       .text("1969")
       .transition().duration(5000)
       .tween("text", function() {
@@ -55,19 +56,24 @@ d3.csv("data.csv", ({year, deficits, unemployment}) => ({year: +year, deficit: +
             return function(t) { that.text(Math.round(i(t))); };
           })
 
+    // chart.append("circle").datum(data[0]).attr("cx", d => x(d.unemployment)).attr("cy", d => y(d.deficit)).attr("r", 5)
+    //   .transition()
+    //   .attr("z", l)
+    //   .attr("cx", function(){ console.log(d3.select(this).attr("z"))})
+
     d3.select("button").attr("disabled", "disabled")
     .on("click", function () {
 
       const button = d3.select(this).attr("disabled", "disabled")
 
-      d3.select("path#dataline")
+      chart.select("path#dataline")
         .attr("stroke-dasharray", `0,${l}`)
         .transition()
         .duration(5000)
         .attr("stroke-dasharray", `${l},${l}`)
         .on("end", () => button.attr("disabled", null))
 
-      d3.select("text#yearDisplay")
+      chart.selectAll(".yearDisplay")
         .text("1969")
         .transition()
         .duration(5000)
